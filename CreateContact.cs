9b2e28a140606d1bf3a15b5e2815d271276e471b
25081997pc <chavanpooja2508@gmail.com>
2022-07-20 14:58:41 +0530

    Editing the person details

--- CreateContact.cs
+++ CreateContact.cs
@@ -29,16 +29,15 @@ namespace AddressBook_Program
             Console.Write("Enter Email: ");
             person.Email = Console.ReadLine();
 
-            string[] addresses = new string[4];
             Console.Write("Enter Area: ");
-            addresses[0] = Console.ReadLine();
+            person.Area = Console.ReadLine();
             Console.Write("Enter City: ");
-            addresses[1] = Console.ReadLine();
+            person.City = Console.ReadLine();
             Console.Write("Enter State: ");
-            addresses[2] = Console.ReadLine();
+            person.State = Console.ReadLine();
             Console.Write("Enter Zip: ");
-            addresses[3] = Console.ReadLine();
-            person.Addresses = addresses;
+            person.Zip = Console.ReadLine();
+           
             People.Add(person);
 
         }
@@ -49,10 +48,10 @@ namespace AddressBook_Program
             Console.WriteLine("Last Name: " + person.LastName);
             Console.WriteLine("Phone Number: " + person.PhoneNumber);
             Console.WriteLine("Email: " + person.Email);
-            Console.WriteLine("Address: Area " + person.Addresses[0]);
-            Console.WriteLine("City: " + person.Addresses[1]);
-            Console.WriteLine("State: " + person.Addresses[2]);
-            Console.WriteLine("Zip: " + person.Addresses[3]);
+            Console.WriteLine("Address: Area " + person.Area);
+            Console.WriteLine("City: " + person.City);
+            Console.WriteLine("State: " + person.State);
+            Console.WriteLine("Zip: " + person.Zip);
             Console.WriteLine("--------------------------------");
 
         }
@@ -66,12 +65,73 @@ namespace AddressBook_Program
                 return;
             }
             Console.WriteLine("Here are the current people in your address book:\n");
-            foreach(var person in People)
+            for(int i = 0; i < People.Count; i++ )
             {
-                PrintPerson(person);
+                PrintPerson(People[i]);
             }
             Console.WriteLine("\nPress any key to continue.");
             Console.ReadKey();
+        }
+        public void Edit()
+        {
+            if (People.Count != 0)
+            {
+                string edit = Console.ReadLine();
+                foreach (var person in People)
+                {
+                    PrintPerson(person);
+                    if (person.FirstName.ToLower() == edit.ToLower())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Changes");
+                            Console.WriteLine("Enter the number:");
+                            int number = Convert.ToInt32(Console.ReadLine());
+                            switch (number)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter New First name: ");
+                                    person.FirstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter New Last name: ");
+                                    person.LastName = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter New Phone Number: ");
+                                    person.PhoneNumber = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter New Area: ");
+                                    person.Area = Console.ReadLine();
+                                    Console.WriteLine("Enter New City: ");
+                                    person.City = Console.ReadLine();
+                                    Console.WriteLine("Enter New State: ");
+                                    person.State = Console.ReadLine();
+                                    Console.WriteLine("Enter New Zip: ");
+                                    person.Zip = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    AddPerson();
+                                    break;
+                                default:
+                                    Console.WriteLine("InValid Number");
+                                    break;
+                            }
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("Enter valid name:");
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Address book is Empty");
+            }
 
         }
     }
@@ -82,6 +142,9 @@ namespace AddressBook_Program
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public string[] Addresses { get; set; }
+        public string Area { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
     }
 }
